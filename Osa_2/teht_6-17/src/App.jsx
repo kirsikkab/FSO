b2702599669e80f87b1d8339b69fa64b670aabf0
kirsikkab <kirsikka.bister@gmail.com>
2026-04-26 10:07:14 +0300

    teht 19

--- Osa_2/teht_6-17/src/App.jsx
+++ Osa_2/teht_6-17/src/App.jsx
@@ -82,14 +82,17 @@ const App = () => {
     }
 
     phonebookService
-      .create(personObject)
-      .then(returnedPerson => {
-        setPersons(persons.concat(returnedPerson))
-        setNewName('')
-        setNewNumber('')
+    .create(personObject)
+    .then(returnedPerson => {
+      setPersons(persons.concat(returnedPerson))
+      setNewName('')
+      setNewNumber('')
 
-        showMessage(`Added ${returnedPerson.name}`, 'success')
-      })
+      showMessage(`Added ${returnedPerson.name}`, 'success')
+    })
+    .catch(error => {
+      showMessage(error.response.data.error, 'error')
+    })
 }
 
   const handleDelete = (id) => {
--- Osa_3/teht_1-6/models/person.js
+++ Osa_3/teht_1-6/models/person.js
@@ -15,15 +15,14 @@ mongoose.connect(url, { family: 4 })
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-})
-
-personSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    required: true
   }
 })
 
