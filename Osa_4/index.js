208feccd1ef8495631b7be03b0d2b90471fe9e01
kirsikkab <kirsikka.bister@gmail.com>
2026-05-01 11:34:03 +0300

    teht 4.2

--- Osa_4/index.js
+++ Osa_4/index.js
@@ -1,46 +1,7 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const app = express()
-
-// Schema
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-// MongoDB
-const mongoUrl = process.env.MONGODB_URI
-
-mongoose.connect(mongoUrl, { family: 4 })
-  .then(() => console.log('connected to MongoDB'))
-  .catch(error => console.log('error:', error.message))
-
-app.use(express.json())
-
-// GET
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then(blogs => {
-    response.json(blogs)
-  })
-})
-
-// POST
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then(result => {
-    response.status(201).json(result)
-  })
-})
-
-const PORT = process.env.PORT
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
---
+++ Osa_4/utils/middleware.js
@@ -0,0 +1,21 @@
+const logger = require('./logger')
+
+const requestLogger = (req, res, next) => {
+  logger.info(req.method, req.path)
+  next()
+}
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, req, res, next) => {
+  logger.error(error.message)
+  next(error)
+}
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler
+}
