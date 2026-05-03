9c160ff8bbe7ee44d54328d3eefd14d0c93239a4
kirsikkab <kirsikka.bister@gmail.com>
2026-05-03 10:28:35 +0300

    teht 4.5

--- Osa_4/tests/list_helper.test.js
+++ Osa_4/tests/list_helper.test.js
@@ -10,6 +10,7 @@ test('dummy returns one', () => {
   assert.strictEqual(result, 1)
 })
 
+//total likes
 describe('total likes', () => {
   const listWithOneBlog = [
     {
@@ -59,5 +60,60 @@ describe('total likes', () => {
   test('of a bigger list is calculated right', () => {
     const result = listHelper.totalLikes(listWithManyBlogs)
     assert.strictEqual(result, 15)
+  })
+})
+
+//favorite blog
+describe('favorite blog', () => {
+  const listWithOneBlog = [
+    {
+      _id: '1',
+      title: 'Only blog',
+      author: 'Author',
+      url: 'http://test.com',
+      likes: 5
+    }
+  ]
+
+  const listWithManyBlogs = [
+    {
+      _id: '1',
+      title: 'Blog 1',
+      author: 'Author 1',
+      url: 'http://test1.com',
+      likes: 5
+    },
+    {
+      _id: '2',
+      title: 'Blog 2',
+      author: 'Author 2',
+      url: 'http://test2.com',
+      likes: 10
+    },
+    {
+      _id: '3',
+      title: 'Blog 3',
+      author: 'Author 3',
+      url: 'http://test3.com',
+      likes: 7
+    }
+  ]
+
+  test('of empty list is null', () => {
+    const result = listHelper.favoriteBlog([])
+    assert.strictEqual(result, null)
+  })
+
+  test('when list has only one blog returns that blog', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    assert.deepStrictEqual(result, listWithOneBlog[0])
+  })
+
+  test('of a bigger list returns blog with most likes', () => {
+    const result = listHelper.favoriteBlog(listWithManyBlogs)
+
+    const expected = listWithManyBlogs[1]
+
+    assert.deepStrictEqual(result, expected)
   })
 })
