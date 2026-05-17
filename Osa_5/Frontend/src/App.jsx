0dd31c370fa8b3eab9f6b92e8fddc9d03e5913bd
kirsikkab <kirsikka.bister@gmail.com>
2026-05-17 11:16:43 +0300

    teht 5.5

--- Osa_5/Frontend/src/App.jsx
+++ Osa_5/Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import Notification from './components/Notification'
+import Togglable from './components/Togglable'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -144,37 +145,39 @@ const App = () => {
         <button onClick={handleLogout}>logout</button>
       </p>
 
-      <h2>Create new</h2>
+      <Togglable buttonLabel="create new blog">
+        <h2>Create new</h2>
 
-      <form onSubmit={addBlog}>
-        <div>
-          title:
-          <input
-            value={newTitle}
-            onChange={({ target }) => setNewTitle(target.value)}
-          />
-        </div>
+        <form onSubmit={addBlog}>
+          <div>
+            title:
+            <input
+              value={newTitle}
+              onChange={({ target }) => setNewTitle(target.value)}
+            />
+          </div>
 
-        <div>
-          author:
-          <input
-            value={newAuthor}
-            onChange={({ target }) => setNewAuthor(target.value)}
-          />
-        </div>
+          <div>
+            author:
+            <input
+              value={newAuthor}
+              onChange={({ target }) => setNewAuthor(target.value)}
+            />
+          </div>
 
-        <div>
-          url:
-          <input
-            value={newUrl}
-            onChange={({ target }) => setNewUrl(target.value)}
-          />
-        </div>
+          <div>
+            url:
+            <input
+              value={newUrl}
+              onChange={({ target }) => setNewUrl(target.value)}
+            />
+          </div>
 
-        <button type="submit">
-          create
-        </button>
-      </form>
+          <button type="submit">
+            create
+          </button>
+        </form>
+      </Togglable>
 
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
