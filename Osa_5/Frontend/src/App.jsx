821763de26598df84b9e5daf2fde822b5c4dccd2
kirsikkab <kirsikka.bister@gmail.com>
2026-05-31 09:24:00 +0300

    teht 5.10

--- Osa_5/Frontend/src/App.jsx
+++ Osa_5/Frontend/src/App.jsx
@@ -161,12 +161,15 @@ const App = () => {
         <BlogForm createBlog={addBlog} />
       </Togglable>
 
-      {blogs.map(blog =>
-        <Blog
-          key={blog.id}
-          blog={blog}
-          handleLike={handleLike}
-        />
+      {blogs
+        .slice()
+        .sort((a, b) => b.likes - a.likes)
+        .map(blog =>
+          <Blog
+            key={blog.id}
+            blog={blog}
+            handleLike={handleLike}
+          />
       )}
     </div>
   )
