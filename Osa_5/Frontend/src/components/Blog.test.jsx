5da6c4c8f5642dad4d3ee05eda3278b151033e6d
kirsikkab <kirsikka.bister@gmail.com>
2026-06-06 09:21:18 +0300

    teht 5.14

--- Osa_5/Frontend/src/components/Blog.test.jsx
+++ Osa_5/Frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders title but not url or likes by default', () => {
@@ -33,4 +34,43 @@ test('renders title but not url or likes by default', () => {
   const likesElement = screen.queryByText('likes 7')
 
   expect(likesElement).toBeNull()
+})
+
+
+test('shows url, likes and author when view button is clicked', async () => {
+  const blog = {
+    title: 'React toimii',
+    author: 'M. Koiranen',
+    url: 'https://example.com',
+    likes: 7,
+    user: {
+      username: 'wuffe',
+      name: 'Minni Koiranen',
+    }
+  }
+
+  render(
+    <Blog blog={blog} />
+  )
+
+  const user = userEvent.setup()
+
+  const button = screen.getByText('view')
+
+  await user.click(button)
+
+  // url näkyy
+  expect(
+    screen.getByText('https://example.com')
+  ).toBeDefined()
+
+  // likes näkyy
+  expect(
+    screen.getByText('likes 7')
+  ).toBeDefined()
+
+  // author näkyy
+  expect(
+    screen.getByText('M. Koiranen')
+  ).toBeDefined()
 })
